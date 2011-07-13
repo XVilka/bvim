f1c1ae78ddb622dc48dda753af5a25637d49b5ef
Anton Kochkov <anton.kochkov@gmail.com>
2011-07-13 23:37:02 +0400

    Beautified code: cmd "indent -npro -kr -i8 -ts8 -sob -l80 -ss -ncs *.[ch]" result. Stage 2

--- bm_dos.c
+++ bm_dos.c
@@ -21,72 +21,88 @@
  * General Public License for more details.
  *
  * See file COPYING for information on distribution conditions.
- */  
-    
+ */
+
 /*
 #define DEBUG 1
-*/ 
-    
+*/
+
 #include "bmore.h"
-    
+
 #define TBUFSIZ 1024
- extern off_t bytepos, screen_home;
-extern FILE *curr_file;
+extern off_t bytepos, screen_home;
+extern FILE *curr_file;
 
 /*
 extern	int		screenlines;
-*/ 
-int got_int;
-int no_intty, no_tty;
- 
-#ifdef DEBUG
-    FILE * dbug;
+*/
+int got_int;
+int no_intty, no_tty;
 
-#endif				/*  */
- void initterm() 
+#ifdef DEBUG
+FILE *dbug;
+
+#endif				/*  */
+void initterm()
 {
-	maxx = 80;
-	maxy = 25;
-}  void set_tty() 
+	maxx = 80;
+	maxy = 25;
+}
+
+void set_tty()
 {
-}  void reset_tty() 
+}
+
+void reset_tty()
 {
-}   void sig() 
+}
+
+void sig()
 {
-	signal(SIGINT, sig);
-	 got_int = TRUE;
-}   
+	signal(SIGINT, sig);
+	got_int = TRUE;
+}
 
 /*
  * doshell() - run a command or an interactive shell
- */ 
-void doshell(cmd)  char *cmd;
+ */
+void doshell(cmd)
+char *cmd;
+{
+	system(cmd);
+	printf("\r");
+	clearscreen();
+	fseek(curr_file, screen_home, SEEK_SET);
+	bytepos = screen_home;
+}
 
+void highlight()
 {
-	system(cmd);
-	printf("\r");
-	clearscreen();
-	fseek(curr_file, screen_home, SEEK_SET);
-	bytepos = screen_home;
-}   void highlight() 
-{
-	highvideo();
-}   void normal() 
-{
-	normvideo();
-}   void clearscreen() 
-{
-	clrscr();
-}   void home() 
-{
-	gotoxy(1, 1);
-}   
+	highvideo();
+}
 
-/* force clear to end of line */ 
-    cleartoeol() 
+void normal()
 {
-	clreol();
-}   int vgetc() 
+	normvideo();
+}
+
+void clearscreen()
 {
-	return ((char)bioskey(0));
-}   
+	clrscr();
+}
+
+void home()
+{
+	gotoxy(1, 1);
+}
+
+/* force clear to end of line */
+void cleartoeol()
+{
+	clreol();
+}
+
+int vgetc()
+{
+	return ((char)bioskey(0));
+}
